e509a5ba9bfb791a8cbb1d0e11a78797e22f84ca
superpasses <165167006+superpasses@users.noreply.github.com>
2024-06-29 16:05:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 29726755
+API_HASH     = "0a708211226ff747f38c47a80a5e29c0"
+BOT_TOKEN    = "6733884564:AAGU2PcodLfQiNy8_wY0ahp6EAD5WFWAB5g"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+DATABASE_URI = "mongodb+srv://deepchutia95:tmpGRbjdJE1WTpG8@cluster0.wrwzavb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002155169038
+ADMIN        = 2107586423
+CHANNEL      = "@Asian_Drama_Central"
